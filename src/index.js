0fb3bd19118c770634a6610e20ab179667c21ae7
allenjustinblanco <blanco.allenjustin@gmail.com>
2018-12-20 13:57:15 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const $ = require('jquery');
+
+import {message} from "./say-hello";
+console.log(message());
+const sayHello = () => {console.log('Hello!')};
+sayHello();
---
+++ src/say-hello.js
@@ -0,0 +1,2 @@
+const $ = require('jquery');
+export const message = () => console.log('hello');
